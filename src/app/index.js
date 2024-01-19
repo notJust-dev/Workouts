6d00fc07ea9cbab963a360c701cf8ace20f2b05b
Vadim Savin <savinvadim1312@gmail.com>
2024-01-19 16:17:04 +0000

    setup expo router

--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
-import exercises from './assets/data/exercises.json';
-import ExerciseListItem from './src/components/ExerciseListItem';
+import exercises from '../../assets/data/exercises.json';
+import ExerciseListItem from '../../src/components/ExerciseListItem';
 
 export default function App() {
   return (
@@ -21,9 +21,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'gainsboro',
+    backgroundColor: 'ghostwhite',
     justifyContent: 'center',
     padding: 10,
-    paddingTop: 70,
   },
 });
--- src/components/ExerciseListItem.jsx
+++ src/components/ExerciseListItem.jsx
@@ -18,6 +18,18 @@ const styles = StyleSheet.create({
     padding: 10,
     borderRadius: 10,
     gap: 5,
+    marginHorizontal: 2,
+
+    // shadow
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+
+    elevation: 2,
   },
   exerciseName: {
     fontSize: 20,
