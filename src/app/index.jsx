b69e474c01420ddc60ddc85901c07468383075ec
Vadim Savin <savinvadim1312@gmail.com>
2024-01-23 18:16:48 +0000

    Search exercises

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -11,8 +11,10 @@ import ExerciseListItem from '../components/ExerciseListItem';
 import { useInfiniteQuery } from '@tanstack/react-query';
 import { gql } from 'graphql-request';
 import client from '../graphqlClient';
-import { Redirect } from 'expo-router';
+import { Redirect, Stack } from 'expo-router';
 import { useAuth } from '../providers/AuthContext';
+import { useState } from 'react';
+import { useDebounce } from '@uidotdev/usehooks';
 
 const exercisesQuery = gql`
   query exercises($muscle: String, $name: String, $offset: Int) {
@@ -25,11 +27,17 @@ const exercisesQuery = gql`
 `;
 
 export default function ExercisesScreen() {
+  const [search, setSearch] = useState('');
+  const debouncedSearchTerm = useDebounce(search.trim(), 1000);
+
   const { data, isLoading, error, fetchNextPage, isFetchingNextPage } =
     useInfiniteQuery({
-      queryKey: ['exercises'],
+      queryKey: ['exercises', debouncedSearchTerm],
       queryFn: ({ pageParam }) =>
-        client.request(exercisesQuery, { offset: pageParam }),
+        client.request(exercisesQuery, {
+          offset: pageParam,
+          name: debouncedSearchTerm,
+        }),
       initialPageParam: 0,
       getNextPageParam: (lastPage, pages) => pages.length * 10,
     });
@@ -60,6 +68,16 @@ export default function ExercisesScreen() {
 
   return (
     <View style={styles.container}>
+      <Stack.Screen
+        options={{
+          headerSearchBarOptions: {
+            placeholder: 'Search...',
+            onChangeText: (event) => setSearch(event.nativeEvent.text),
+            hideWhenScrolling: false,
+          },
+        }}
+      />
+
       <FlatList
         data={exercises}
         contentContainerStyle={{ gap: 5 }}
@@ -69,7 +87,6 @@ export default function ExercisesScreen() {
         onEndReached={loadMore}
       />
 
-      {/* <Button title="Load more" onPress={fetchNextPage} /> */}
       <StatusBar style="auto" />
     </View>
   );
@@ -80,5 +97,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     padding: 10,
+    paddingTop: 150,
   },
 });
