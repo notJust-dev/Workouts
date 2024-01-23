a42bc39764f3883a58fa1899fbb1eb96e660d60a
Vadim Savin <savinvadim1312@gmail.com>
2024-01-23 16:43:58 +0000

    Invalidate sets query when adding a new set

--- src/components/NewSetInput.jsx
+++ src/components/NewSetInput.jsx
@@ -1,7 +1,7 @@
 import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 import { useState } from 'react';
 import { gql } from 'graphql-request';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import graphqlClient from '../graphqlClient';
 
 const mutationDocument = gql`
@@ -21,13 +21,18 @@ const NewSetInput = ({ exerciseName }) => {
   const [reps, setReps] = useState('');
   const [weight, setWeight] = useState('');
 
+  const queryClient = useQueryClient();
+
   const { mutate, error, isError, isPending } = useMutation({
     mutationFn: (newSet) => graphqlClient.request(mutationDocument, { newSet }),
+    onSuccess: () => {
+      setReps('');
+      setWeight('');
+      queryClient.invalidateQueries({ queryKey: ['sets', exerciseName] });
+    },
   });
 
   const addSet = () => {
-    console.warn('Add set: ', reps, weight);
-
     const newSet = {
       exercise: exerciseName,
       reps: Number.parseInt(reps),
@@ -36,9 +41,6 @@ const NewSetInput = ({ exerciseName }) => {
       newSet.weight = Number.parseFloat(weight);
     }
     mutate(newSet);
-
-    setReps('');
-    setWeight('');
   };
 
   console.log(error);
